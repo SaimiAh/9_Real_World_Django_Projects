06f094f597acd73f4bcbcbfb6ebc641ba2d3c4c5
SaimiAh <ah.saim786@gmail.com>
2023-07-20 14:39:20 +0500

    Removed Practice Project

--- My_DjP3/enroll/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from enroll import views
-urlpatterns = [
-    path('stu/', views.stuinfo),
-]
--- My_DjP3/enroll/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from enroll.models import Student
- 
-def stuinfo(request):
-    stud=Student.objects.all()
-    return render(request, 'enroll/student.html', {'stu' : stud})
