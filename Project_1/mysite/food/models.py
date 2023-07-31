a9d0990061b9ec0a2c4e4b9f72ed878a9160009f
SaimiAh <ah.saim786@gmail.com>
2023-07-31 14:53:37 +0500

    Project 1

--- Project_1/mysite/food/models.py
+++ Project_1/mysite/food/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
 class item(models.Model):
+    def __str__(self):
+        return self.item_name
+
     item_name = models.CharField(max_length=100)
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
