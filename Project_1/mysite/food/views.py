f647b417acbb4e157cccd805518cb1513f4150ee
SaimiAh <ah.saim786@gmail.com>
2023-08-04 15:46:35 +0500

    Used Template Rendering and Updated Views

--- Project_1/mysite/food/views.py
+++ Project_1/mysite/food/views.py
@@ -7,9 +7,9 @@ def index(request):
     item_list = Item.objects.all()
     template = loader.get_template('food/index.html')
     context  = {
-        
+        'item_list':item_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
 
 def item(request):
     return HttpResponse("This is the item.")
