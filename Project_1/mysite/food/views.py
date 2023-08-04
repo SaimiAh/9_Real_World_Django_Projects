952cdc001a30e0a7dc6bbba295d10c984c6849aa
SaimiAh <ah.saim786@gmail.com>
2023-08-04 14:14:00 +0500

    Same Model Name Error and Template Loader Test.

--- Project_1/mysite/food/views.py
+++ Project_1/mysite/food/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from food.models import Item
+from django.template import loader
 
 def index(request):
-    return HttpResponse("hello")
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context  = {
+        
+    }
+    return HttpResponse(template.render(context, request))
 
 def item(request):
     return HttpResponse("This is the item.")
