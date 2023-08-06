7d872c0639407c1d89963f7b22b1a7feb2e6b0df
SaimiAh <ah.saim786@gmail.com>
2023-08-06 17:43:34 +0500

    Project 1 Modified files.

--- Project_1/mysite/food/models.py
+++ Project_1/mysite/food/models.py
@@ -8,3 +8,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=100)
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=1000, default="https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg")
