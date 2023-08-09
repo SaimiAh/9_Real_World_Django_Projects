23c4cc623852deba4501f8848a11c137ace471a8
SaimiAh <ah.saim786@gmail.com>
2023-08-09 18:59:14 +0500

    Created Views CRUD

--- Project_1/mysite/food/urls.py
+++ Project_1/mysite/food/urls.py
@@ -2,8 +2,16 @@ from . import views
 from django.urls import path
 app_name = 'food'
 urlpatterns = [
+    #/food/ home url
     path('', views.index, name='index'),
+    #/food/1 item detail url
     path('<int:item_id>/', views.details, name='details'),
+    #item
     path('item/', views.item, name='item'),
-    path('add', views.create_item, name='create_item')
+    #/food/add additem 
+    path('add/', views.create_item, name='create_item'),
+    #/food/update/1 upadate a specific item
+    path('update/<int:id>/', views.update_item, name='update_item'),
+    #/ delete a specific item
+    path('delete/<int:id>/', views.delete_item, name='deletes_item'),
 ]
--- Project_1/mysite/food/views.py
+++ Project_1/mysite/food/views.py
@@ -4,6 +4,8 @@ from food.models import Item
 from django.template import loader
 from .forms import ItemForm
 
+
+#HOME VIEW
 def index(request):
     item_list = Item.objects.all()
     template = loader.get_template('food/index.html')
@@ -12,16 +14,13 @@ def index(request):
     }
     return render(request, 'food/index.html', context)
 
+
+#NULL VIEW
 def item(request):
     return HttpResponse("This is the item.")
 
-def details(request,item_id):
-    item_details=Item.objects.get(pk=item_id)
-    context  = {
-        'item_details':item_details,
-    }
-    return render(request, 'food/details.html', context)
 
+#CREATE VIEW
 def create_item(request):
     form = ItemForm(request.POST or None)
 
@@ -31,3 +30,32 @@ def create_item(request):
     return render(request, 'food/item-form.html', {'form':form})
 
 
+#READ VIEW
+def details(request,item_id):
+    item_details=Item.objects.get(pk=item_id)
+    context  = {
+        'item_details':item_details,
+    }
+    return render(request, 'food/details.html', context)
+
+
+# UPADTE VIEW
+def update_item(request,id):
+    item = Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request, 'food/item-form.html', {'form':form, 'item':item,})
+
+
+# DELETE VIEW 
+def delete_item(request,id):
+    item = Item.objects.get(id=id)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-delete.html', {'item':item})
