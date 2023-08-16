25afd0290492e4bc957a8bb08ae770fe5fadf627
SaimiAh <ah.saim786@gmail.com>
2023-08-16 20:26:41 +0500

    DRF and use of Serializers.

--- Project_1/drfsite/movies/views.py
+++ Project_1/drfsite/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MovieDataSerializer
+from .models import MovieData
 
-# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = MovieData.objects.all()
+    serializer_class = MovieDataSerializer
+
+
