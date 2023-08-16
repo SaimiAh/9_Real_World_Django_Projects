081f5ffc34a953bca712793ecc961a959e6aec6d
SaimiAh <ah.saim786@gmail.com>
2023-08-16 18:48:58 +0500

    DRF and use of Serializers.

---
+++ Project_1/drfsite/movies/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class MovieData(models.Model):
+    def __str__(self) -> str:
+        return self.name
+
+    name = models.CharField(max_length=100)
+    duration = models.FloatField()
+    rating = models.FloatField()
