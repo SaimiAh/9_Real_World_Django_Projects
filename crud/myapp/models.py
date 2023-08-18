5ae9bac9c2425866c335b154d2ec157745e281be
SaimiAh <ah.saim786@gmail.com>
2023-08-18 15:57:26 +0500

    Crud Deleted

--- crud/myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-    rollnumber = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
