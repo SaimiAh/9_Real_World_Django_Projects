ed9a570addf5969860dd0aee9e10a3a3ac1b7fa5
SaimiAh <ah.saim786@gmail.com>
2023-08-25 17:43:58 +0500

    Created Model for Products

--- Project_2/ecomsite/shop/models.py
+++ Project_2/ecomsite/shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=300)
+    price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField(max_length=300)
+    description = models.TextField()
+    image = models.CharField(max_length=300)
