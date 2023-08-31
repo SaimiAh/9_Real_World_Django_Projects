feda2f3de50e933bdd391eb524fdc3d10b527c0d
SaimiAh <ah.saim786@gmail.com>
2023-08-31 16:49:28 +0500

    Search Bar View functionality is written.

--- Project_2/ecomsite/shop/views.py
+++ Project_2/ecomsite/shop/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from .models import Products
 def index(request):
     product_objects = Products.objects.all()
+    item_name = request.Get.get('item_name')
+    if item_name!= '' and item_name is not None:
+        product_objects = Products.objects.filter(title_icontains = item_name)
     return render(request, 'shop/index.html', {'product_objects': product_objects})
