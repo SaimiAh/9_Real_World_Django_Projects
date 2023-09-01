29b09e910285f8294a7e6d0193d95cf868ade028
SaimiAh <ah.saim786@gmail.com>
2023-09-01 17:28:32 +0500

    Added Pagination in views.

--- Project_2/ecomsite/shop/views.py
+++ Project_2/ecomsite/shop/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Products
+from django.core.paginator import Paginator
 def index(request):
     product_objects = Products.objects.all()
-    item_name = request.Get.get('item_name')
-    if item_name!= '' and item_name is not None:
-        product_objects = Products.objects.filter(title_icontains = item_name)
-    return render(request, 'shop/index.html', {'product_objects': product_objects})
+ 
+    #search code
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(title__icontains=item_name)
+ 
+    #paginator code
+    paginator = Paginator(product_objects,4)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+    
+    return render(request,'shop/index.html',{'product_objects':product_objects})
