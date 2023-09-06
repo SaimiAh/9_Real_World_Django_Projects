1ac860a82df233e60c451c43610cb67c3c6ce3eb
SaimiAh <ah.saim786@gmail.com>
2023-09-06 15:35:29 +0500

    Created Order Model.

--- Project_2/ecomsite/ecomsite/urls.py
+++ Project_2/ecomsite/ecomsite/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('<int:id>/',views.detail,name='detail'),
-    path('checkout/',views.checkout,name='checkout'),
+    #path('checkout/',views.checkout,name='checkout'),
    
 ]
