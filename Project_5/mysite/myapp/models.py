8d87bbbbf7342535640226f076b6f71db0aab2d3
SaimiAh <ah.saim786@gmail.com>
2023-09-09 23:40:17 +0500

    Calorie Counter: Created Food and Consume Model and Migrated.

--- Project_5/mysite/myapp/models.py
+++ Project_5/mysite/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Food(models.Model):
     def __str__(self) -> str:
@@ -8,3 +9,7 @@ class Food(models.Model):
     protien = models.FloatField()
     fats = models.FloatField()
     calorie =  models.FloatField()
+
+class Consume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food, on_delete=models.CASCADE)
