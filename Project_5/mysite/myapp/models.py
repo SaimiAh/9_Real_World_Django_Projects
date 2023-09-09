59c62faf31e3211a9e6d88c49dcaf180dee53a4b
SaimiAh <ah.saim786@gmail.com>
2023-09-09 22:10:35 +0500

    Calorie Counter: Model Updated.

--- Project_5/mysite/myapp/models.py
+++ Project_5/mysite/myapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Food(models.Model):
+    def __str__(self) -> str:
+        return self.name
     name = models.CharField(max_length=200)
     carbs =  models.FloatField()
     protien = models.FloatField()
