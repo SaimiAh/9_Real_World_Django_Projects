66780112b6c9ac5b3f31318e89e831e14fe61db2
SaimiAh <ah.saim786@gmail.com>
2023-09-11 21:12:54 +0500

    Social Media Site: Code  Updated.

--- Project_6/socialproject/users/urls.py
+++ Project_6/socialproject/users/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 from re import template
 from users import views
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('', views.index, name='index'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('login/', views.user_login, name='login'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name = 'users/password_change_form.html'), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeView.as_view(template_name = 'users/password_change_done.html'), name='password_change_done'),
-
+    path('password_change/', auth_view.PasswordChangeView.as_view(template_name = 'users/password_change_form.html'), name='password_change'),
+    path('password_change/done/', auth_view.PasswordChangeView.as_view(template_name = 'users/password_change_done.html'), name='password_change_done'),
+    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+         name='password_reset'),
+    path('password_reset/done', auth_view.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_view.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_view.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
