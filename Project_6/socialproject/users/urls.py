d32833e620a4e5629d9897db0bc0799f85e1a51c
SaimiAh <ah.saim786@gmail.com>
2023-09-11 20:26:53 +0500

    Social Media Site: Login and Logout Url Updated.

--- Project_6/socialproject/users/urls.py
+++ Project_6/socialproject/users/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('login/', views.user_login, name='login'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name = 'users/password_change_form.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeView.as_view(template_name = 'users/password_change_done.html'), name='password_change_done'),
+
 ]
