ce7f7a90cc5fb46aff9840b486425ef4f6c476ec
SaimiAh <ah.saim786@gmail.com>
2023-09-11 19:03:59 +0500

    Social Media Site: Added Logout functionality.

--- Project_6/socialproject/users/urls.py
+++ Project_6/socialproject/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from re import template
 from users import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('login/', views.user_login, name='login'),
 ]
