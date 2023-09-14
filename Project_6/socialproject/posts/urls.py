be2c9a0d933cf074212e77021b555ae1bd5d81e5
SaimiAh <ah.saim786@gmail.com>
2023-09-14 17:08:06 +0500

    Social Media Site: Post Url udated.

--- Project_6/socialproject/posts/urls.py
+++ Project_6/socialproject/posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('create/', views.post_create, name='create'),
-    #path('feed', views.feed, name='feed'),
+    path('feed', views.feed, name='feed'),
     #path('like', views.like_post, name='like'),
 ]
