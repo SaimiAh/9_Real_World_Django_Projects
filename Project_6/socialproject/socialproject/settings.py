d8d3081607e7c2ecd54ad0087b4bbdc4d443e211
SaimiAh <ah.saim786@gmail.com>
2023-09-14 19:22:50 +0500

    Social Media Site: code  updated.

--- Project_6/socialproject/socialproject/settings.py
+++ Project_6/socialproject/socialproject/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
-    'posts', 
+    'posts',
+    'mathfilters', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
