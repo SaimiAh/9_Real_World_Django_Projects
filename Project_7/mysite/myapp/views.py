afc199c1c018ce913410bc1435bfd18dc342e57a
SaimiAh <ah.saim786@gmail.com>
2023-09-16 15:02:39 +0500

    Expense Tracker: Code Updated.

--- Project_7/mysite/myapp/views.py
+++ Project_7/mysite/myapp/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render,redirect
 from .forms import ExpenseForm
 from .models import Expense
-from django.db.models import Sum
 import datetime
+from django.db.models import Sum
 # Create your views here.
 def index(request):
     if request.method =="POST":
         expense = ExpenseForm(request.POST)
         if expense.is_valid():
             expense.save()
-            return redirect('index')
-
+            
     expenses = Expense.objects.all()
     total_expenses = expenses.aggregate(Sum('amount'))
-
-       #Logic to calculate 365 days expenses
+    
+    
+    
+    #Logic to calculate 365 days expenses
     last_year = datetime.date.today() - datetime.timedelta(days=365)
     data = Expense.objects.filter(date__gt=last_year)
     yearly_sum = data.aggregate(Sum('amount'))
@@ -28,25 +29,27 @@ def index(request):
     last_week = datetime.date.today() - datetime.timedelta(days=7)
     data = Expense.objects.filter(date__gt=last_week)
     weekly_sum = data.aggregate(Sum('amount'))
-
+    
     daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+    
+    
     categorical_sums = Expense.objects.filter().values('category').order_by('category').annotate(sum=Sum('amount'))
     print(categorical_sums)
     
     expense_form = ExpenseForm()
-    return render(request,'myapp/index.html', {'expense_form': expense_form, 'expenses': expenses, 'total_expenses':total_expenses,'yearly_sum':yearly_sum,'weekly_sum':weekly_sum,'monthly_sum':monthly_sum, 'daily_sums':daily_sums,'categorical_sums':categorical_sums})
+    return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses,'total_expenses':total_expenses,'yearly_sum':yearly_sum,'weekly_sum':weekly_sum,'monthly_sum':monthly_sum,'daily_sums':daily_sums,'categorical_sums':categorical_sums})
 
 def edit(request,id):
-    expense = Expense.objects.get(id = id)
+    expense = Expense.objects.get(id=id)
     expense_form = ExpenseForm(instance=expense)
-    if request.method == 'POST':
-        expense = Expense.objects.get(id = id)
+    if request.method =="POST":
+        expense = Expense.objects.get(id=id)
         form = ExpenseForm(request.POST,instance=expense)
         if form.is_valid():
             form.save()
             return redirect('index')
+        
     return render(request,'myapp/edit.html',{'expense_form':expense_form})
-
 
 def delete(request,id):
     if request.method =='POST' and 'delete' in request.POST:
