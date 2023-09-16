71b9b6d71d9acaa2c52756b9b28bdf00d8570f3f
SaimiAh <ah.saim786@gmail.com>
2023-09-16 14:44:48 +0500

    Expense Tracker: Code Updated.

--- Project_7/mysite/myapp/views.py
+++ Project_7/mysite/myapp/views.py
@@ -29,10 +29,12 @@ def index(request):
     data = Expense.objects.filter(date__gt=last_week)
     weekly_sum = data.aggregate(Sum('amount'))
 
-    daily_sum = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+    daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+    categorical_sums = Expense.objects.filter().values('category').order_by('category').annotate(sum=Sum('amount'))
+    print(categorical_sums)
     
     expense_form = ExpenseForm()
-    return render(request,'myapp/index.html', {'expense_form': expense_form, 'expenses': expenses, 'total_expenses':total_expenses,'yearly_sum':yearly_sum,'weekly_sum':weekly_sum,'monthly_sum':monthly_sum, 'daily_sum':daily_sum})
+    return render(request,'myapp/index.html', {'expense_form': expense_form, 'expenses': expenses, 'total_expenses':total_expenses,'yearly_sum':yearly_sum,'weekly_sum':weekly_sum,'monthly_sum':monthly_sum, 'daily_sums':daily_sums})
 
 def edit(request,id):
     expense = Expense.objects.get(id = id)
