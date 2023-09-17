c4d50e6bbda69b1d8cb6c99a060a6db7705a8d6c
SaimiAh <ah.saim786@gmail.com>
2023-09-17 19:26:59 +0500

    Chat App: Code Updated.

--- Project_8/mysite/mysite/settings.py
+++ Project_8/mysite/mysite/settings.py
@@ -73,6 +73,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 ASGI_APPLICATION = 'mysite.asgi.application'
 
 
+
+CHANNEL_LAYERs = {
+    'default':{
+        'BACKEND':'channels.layers.InMemoryChannelLayer'
+
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
