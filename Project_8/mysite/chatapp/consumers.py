020bc0710eecedbdf569a862eaf6d53cb62abbee
SaimiAh <ah.saim786@gmail.com>
2023-09-18 18:09:10 +0500

    Chat App: Code Updated.

--- Project_8/mysite/chatapp/consumers.py
+++ Project_8/mysite/chatapp/consumers.py
@@ -15,7 +15,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     
     
     
-    async def disconnect(self, code):
+    async def disconnect(self):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name,
