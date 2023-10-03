f573cb67890786bcf5c661798438025b38c553ba
SaimiAh <ah.saim786@gmail.com>
2023-10-03 20:35:17 +0500

    Raw api project

---
+++ Raw_Code/drfcode/myapp.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+URL = ""
+
+data = {
+    'name':'Saim',
+    'rollno':101,
+    'city':'Vehari',
+}
+json_data = json.dump(data)
+r = requests.post(url = URL, data = json_data)
