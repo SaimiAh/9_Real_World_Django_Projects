26c95ab25875a5c0a4028b95c8f0684ab7b05545
SaimiAh <ah.saim786@gmail.com>
2023-10-04 15:26:40 +0500

    Raw api project

--- Raw_Code/drfcode/api/views.py
+++ Raw_Code/drfcode/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .serializers import StudentSerializer
+import io
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+
+def stucreate(request):
+    if request.method == "POST":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser.parse(stream)
+        serializer = StudentSerializer(pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'message':'Data Created.', }
+            
+
